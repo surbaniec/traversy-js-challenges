2b06408c19c4200bc885a17c998adbd06c90ed37
Sebastian Urbaniec <seurbaniec@gmail.com>
2024-03-05 20:36:25 +0100

    challange 1.5 done

--- 01-basic-challenges-1/05-find-max-number/find-max-number.js
+++ 01-basic-challenges-1/05-find-max-number/find-max-number.js
@@ -1,3 +1,11 @@
-function findMaxNumber() {}
+function findMaxNumber(arr) {
+  let largestNumber = arr[0];
+
+  arr.forEach((num) => {
+    num > largestNumber ? (largestNumber = num) : largestNumber;
+  });
+
+  return largestNumber;
+}
 
 module.exports = findMaxNumber;
