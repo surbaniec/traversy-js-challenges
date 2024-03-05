000f961239ce3691b51288a5b5190e4389b4b941
Sebastian Urbaniec <seurbaniec@gmail.com>
2024-03-05 19:55:24 +0100

    challange 1.4 done

--- 01-basic-challenges-1/04-count-occurrences/count-occurrences.js
+++ 01-basic-challenges-1/04-count-occurrences/count-occurrences.js
@@ -1,3 +1,11 @@
-function countOccurrences() {}
+function countOccurrences(str, char) {
+  let occurancesCount = 0;
+
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === char) occurancesCount++;
+  }
+
+  return occurancesCount;
+}
 
 module.exports = countOccurrences;
