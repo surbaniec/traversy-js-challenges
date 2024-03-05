7184ceb3f7134b1888ab4799ca67424bf7760cd4
Sebastian Urbaniec <seurbaniec@gmail.com>
2024-03-05 19:44:25 +0100

    challange 1.3 done

--- 01-basic-challenges-1/03-calculator/calculator.js
+++ 01-basic-challenges-1/03-calculator/calculator.js
@@ -1,3 +1,16 @@
-function calculator() {}
+function calculator(num1, num2, operator) {
+  switch (operator) {
+    case '+':
+      return num1 + num2;
+    case '-':
+      return num1 - num2;
+    case '*':
+      return num1 * num2;
+    case '/':
+      return num1 / num2;
+    default:
+      throw new Error('invalid operator');
+  }
+}
 
 module.exports = calculator;
