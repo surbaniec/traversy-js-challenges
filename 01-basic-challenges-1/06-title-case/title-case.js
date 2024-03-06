8d9d4bc635b52f2da63c5227c3547562cfd986ef
Sebastian Urbaniec <seurbaniec@gmail.com>
2024-03-06 21:07:01 +0100

    challange 1.6 done

--- 01-basic-challenges-1/06-title-case/title-case.js
+++ 01-basic-challenges-1/06-title-case/title-case.js
@@ -1,3 +1,11 @@
-function titleCase() {}
+function titleCase(str) {
+  newStr = str.toLowerCase().split(' ');
+
+  return newStr
+    .map((el) => {
+      return el.charAt(0).toUpperCase() + el.slice(1);
+    })
+    .join(' ');
+}
 
 module.exports = titleCase;
