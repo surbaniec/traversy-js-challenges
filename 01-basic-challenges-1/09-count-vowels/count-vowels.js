afa968983e856b9d2235f29d503fb3d51884abc5
Sebastian Urbaniec <seurbaniec@gmail.com>
2024-03-09 16:55:39 +0100

    challange 1.9 done

--- 01-basic-challenges-1/09-count-vowels/count-vowels.js
+++ 01-basic-challenges-1/09-count-vowels/count-vowels.js
@@ -1,3 +1,31 @@
-function countVowels() {}
+function countVowels(str) {
+  let vowelsCounter = 0;
+
+  for (let i = str.length - 1; i >= 0; i--) {
+    switch (str[i].toLowerCase()) {
+      case 'a':
+        vowelsCounter++;
+        break;
+      case 'e':
+        vowelsCounter++;
+        break;
+      case 'i':
+        vowelsCounter++;
+        break;
+      case 'o':
+        vowelsCounter++;
+        break;
+      case 'u':
+        vowelsCounter++;
+        break;
+      case 'y':
+        vowelsCounter++;
+        break;
+      default:
+        break;
+    }
+  }
+  return vowelsCounter;
+}
 
 module.exports = countVowels;
