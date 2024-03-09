a46fa819863499209a89e32e731b5ab9e6bfbd98
Sebastian Urbaniec <seurbaniec@gmail.com>
2024-03-09 16:37:18 +0100

    challange 1.8 done

--- 01-basic-challenges-1/08-palindrome/palindrome-run.js
+++ 01-basic-challenges-1/08-palindrome/palindrome-run.js
@@ -3,4 +3,4 @@ const isPalindrome = require('./palindrome');
 const result1 = isPalindrome('racecar');
 const result2 = isPalindrome('racecars');
 
-console.log(result1, result2);
+console.log(result1, result2, result3);
--- 01-basic-challenges-1/08-palindrome/palindrome.js
+++ 01-basic-challenges-1/08-palindrome/palindrome.js
@@ -1,3 +1,12 @@
-function isPalindrome() {}
+function isPalindrome(str) {
+  const formatedStr = str.toLowerCase().replace(/[^a-z0-9]/g, '');
+  let reversed = '';
+
+  for (let i = formatedStr.length - 1; i >= 0; i--) {
+    reversed += formatedStr[i];
+  }
+
+  return reversed === formatedStr ? true : false;
+}
 
 module.exports = isPalindrome;
