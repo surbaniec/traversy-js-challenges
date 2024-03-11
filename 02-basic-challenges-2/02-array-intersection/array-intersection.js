c97468dbf100d8bbbd50a6553c6325fb6a9f1a99
Sebastian Urbaniec <seurbaniec@gmail.com>
2024-03-11 20:19:27 +0100

    challange 2.2 done

--- 02-basic-challenges-2/02-array-intersection/array-intersection.js
+++ 02-basic-challenges-2/02-array-intersection/array-intersection.js
@@ -1,3 +1,10 @@
-function arrayIntersection() {}
+function arrayIntersection(arr1, arr2) {
+  const arr1Set = new Set(arr1);
+  const intersection = [];
+  for (let i = 0; i < arr2.length; i++) {
+    if (arr1Set.has(arr2[i])) intersection.push(arr2[i]);
+  }
+  return intersection;
+}
 
 module.exports = arrayIntersection;
