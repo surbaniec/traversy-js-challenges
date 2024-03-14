72bdca37b0c99a43e26a7d832ac46a5d1e589540
Sebastian Urbaniec <seurbaniec@gmail.com>
2024-03-14 20:03:00 +0100

    challange 1.5 done

--- 02-basic-challenges-2/05-find-missing-letter/find-missing-letter.test.js
+++ 02-basic-challenges-2/05-find-missing-letter/find-missing-letter.test.js
@@ -1,4 +1,4 @@
-const findMissingLetter = require('./find-missing-letter');
+const findMissingLetter = require('./find-missing.letter');
 
 test('Find Missing Letter', () => {
   expect(findMissingLetter(['a', 'b', 'c', 'e'])).toBe('d');
