dc326a5630d181f3532f3455506274e199bdc181
Sebastian Urbaniec <seurbaniec@gmail.com>
2024-03-14 20:51:46 +0100

    challange 2.7 done

--- 02-basic-challenges-2/07-first-non-repeating/first-non-repeating.js
+++ 02-basic-challenges-2/07-first-non-repeating/first-non-repeating.js
@@ -1,3 +1,17 @@
-function findFirstNonRepeatingCharacter() {}
+function findFirstNonRepeatingCharacter(str) {
+  const charMap = new Map();
+
+  for (let i = 0; i < str.length; i++) {
+    charMap.set(str[i], (charMap.get(str[i]) || 0) + 1);
+  }
+
+  for (let i = 0; i < str.length; i++) {
+    if (charMap.get(str[i]) === 1) {
+      return str[i];
+    }
+  }
+
+  return null;
+}
 
 module.exports = findFirstNonRepeatingCharacter;
