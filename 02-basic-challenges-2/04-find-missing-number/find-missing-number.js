46d2bcb42c4f22e64d63dd9712f3835368296276
Sebastian Urbaniec <seurbaniec@gmail.com>
2024-03-14 19:35:05 +0100

    challange 1.4 done

--- 02-basic-challenges-2/04-find-missing-number/find-missing-number.js
+++ 02-basic-challenges-2/04-find-missing-number/find-missing-number.js
@@ -1,3 +1,16 @@
-function findMissingNumber() {}
+function findMissingNumber(arr) {
+  if (!arr) return undefined;
+  if (arr.length === 0) return 1;
+
+  let n = arr.length + 1;
+  const nSum = (n * (n + 1)) / 2;
+
+  let actualSum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    actualSum += arr[i];
+  }
+
+  return nSum - actualSum;
+}
 
 module.exports = findMissingNumber;
