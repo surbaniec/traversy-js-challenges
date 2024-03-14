ed9db141363ed6de5a3ff44ea0c8f6d429b6fb01
Sebastian Urbaniec <seurbaniec@gmail.com>
2024-03-14 20:13:28 +0100

    challange 1.6 done

--- 02-basic-challenges-2/06-are-all-chars-unique/are-all-chars-unique.js
+++ 02-basic-challenges-2/06-are-all-chars-unique/are-all-chars-unique.js
@@ -1,3 +1,13 @@
-function areAllCharactersUnique() {}
+function areAllCharactersUnique(str) {
+  const uniqueCharacters = new Set(str);
+  let setLength = 0;
+
+  uniqueCharacters.forEach((el) => {
+    setLength++;
+  });
+
+  if (setLength !== str.length) return false;
+  else return true;
+}
 
 module.exports = areAllCharactersUnique;
