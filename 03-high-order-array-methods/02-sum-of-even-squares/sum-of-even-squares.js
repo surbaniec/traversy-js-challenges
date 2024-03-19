b275c053d471dc49606f847d9d6aa12d07e89755
Sebastian Urbaniec <seurbaniec@gmail.com>
2024-03-19 21:02:10 +0100

    challange 3.2 done

--- 03-high-order-array-methods/02-sum-of-even-squares/sum-of-even-squares.js
+++ 03-high-order-array-methods/02-sum-of-even-squares/sum-of-even-squares.js
@@ -1,3 +1,7 @@
-function sumOfEvenSquares() {}
+function sumOfEvenSquares(numbers) {
+  const evenNumbers = numbers.filter((num) => num % 2 === 0);
+  const squareResults = evenNumbers.map((num) => num * num);
+  return squareResults.reduce((acc, currentValue) => acc + currentValue, 0);
+}
 
 module.exports = sumOfEvenSquares;
