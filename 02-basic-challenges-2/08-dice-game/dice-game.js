3e9c620f8a4409875c0883c8464a902b96448da3
Sebastian Urbaniec <seurbaniec@gmail.com>
2024-03-19 20:01:46 +0100

    challange 2.8 done

--- 02-basic-challenges-2/08-dice-game/dice-game.js
+++ 02-basic-challenges-2/08-dice-game/dice-game.js
@@ -1,3 +1,26 @@
-function diceGameSimulation() {}
+function diceGameSimulation(numSimulations) {
+  const results = [];
+
+  for (let i = 0; i < numSimulations; i++) {
+    const dice1 = Math.floor(Math.random() * 6) + 1;
+    const dice2 = Math.floor(Math.random() * 6) + 1;
+
+    const sum = dice1 + dice2;
+    let result;
+
+    if (sum === 7 || sum === 11) result = 'win';
+    else if (sum === 2 || sum === 3 || sum === 12) result = 'lose';
+    else result = 'roll again';
+
+    results[i] = {
+      dice1,
+      dice2,
+      sum,
+      result,
+    };
+  }
+
+  return results;
+}
 
 module.exports = diceGameSimulation;
