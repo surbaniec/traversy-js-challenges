e429595ef17c07bf5114f374828ac09c840d6fee
Sebastian Urbaniec <seurbaniec@gmail.com>
2024-03-19 20:24:19 +0100

    challange 1.9 done

--- 02-basic-challenges-2/09-format-phone-number/format-phone-number.js
+++ 02-basic-challenges-2/09-format-phone-number/format-phone-number.js
@@ -1,3 +1,10 @@
-function formatPhoneNumber() {}
+function formatPhoneNumber(numbers) {
+  const number = numbers.join('');
+  const firstPiece = number.slice(0, 3);
+  const secondPiece = number.slice(3, 6);
+  const thirdPiece = number.slice(6);
+
+  return `(${firstPiece}) ${secondPiece}-${thirdPiece}`;
+}
 
 module.exports = formatPhoneNumber;
